3620cbe5023d9c3388688fde179af38807e146b4
ketongu10 <ketongu10@gmail.com>
2023-02-14 23:55:53 +0300

    Initial commit

---
+++ Utils/foo.py
@@ -0,0 +1,10 @@
+from ctypes import cdll
+lib = cdll.LoadLibrary('./Utils/libfoo.so')
+
+
+class Foo(object):
+    def __init__(self):
+        self.obj = lib.Foo_new()
+
+    def bar(self):
+        lib.Foo_bar(self.obj)
---
+++ main.py
@@ -0,0 +1,12 @@
+
+import Utils.foo
+
+
+
+
+
+if __name__ == '__main__':
+    f = Utils.foo.Foo()
+    f.bar()  # and you will see "Hello" on the screen
+
+
